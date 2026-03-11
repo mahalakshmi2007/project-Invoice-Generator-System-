9f78e08f207e25bbd3fd56e0b022969420104f1a
mahalakshmi2007 <msrimahalakshmi543@gmail.com>
2026-03-11 14:06:44 +0000

    Add login function for user authentication

---
+++ login.js
@@ -0,0 +1,13 @@
+function login(){
+
+let user = document.getElementById("username").value;
+let pass = document.getElementById("password").value;
+
+if(user === "admin" && pass === "1234"){
+window.location.href = "invoice.html";
+}
+else{
+document.getElementById("error").innerText="Invalid Login";
+}
+
+}
